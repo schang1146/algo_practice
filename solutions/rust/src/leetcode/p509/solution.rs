21151b6067952e3263e9e7f41849bdc4253b3b02
schang1146 <sammychang2185@gmail.com>
2022-10-28 20:09:47 -0700

    Add tests

--- solutions/rust/src/leetcode/p509/solution.rs
+++ solutions/rust/src/leetcode/p509/solution.rs
@@ -18,3 +18,34 @@ impl Solution {
         return fib_data[2]
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn small_fib() {
+        assert_eq!(0, Solution::fib(0));
+        assert_eq!(1, Solution::fib(1));
+        assert_eq!(1, Solution::fib(2));
+        assert_eq!(2, Solution::fib(3));
+        assert_eq!(3, Solution::fib(4));
+        assert_eq!(5, Solution::fib(5));
+        assert_eq!(8, Solution::fib(6));
+        assert_eq!(13, Solution::fib(7));
+        assert_eq!(21, Solution::fib(8));
+    }
+
+    #[test]
+    fn big_fib() {
+        assert_eq!(39088169, Solution::fib(38));
+        assert_eq!(63245986, Solution::fib(39));
+        assert_eq!(102334155, Solution::fib(40));
+        assert_eq!(165580141, Solution::fib(41));
+        assert_eq!(267914296, Solution::fib(42));
+        assert_eq!(433494437, Solution::fib(43));
+        assert_eq!(701408733, Solution::fib(44));
+        assert_eq!(1134903170, Solution::fib(45));
+        assert_eq!(1836311903   , Solution::fib(46));
+    }
+}
