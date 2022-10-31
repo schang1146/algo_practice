28af460259f844aa27e52a984573f045dabd0182
schang1146 <sammychang2185@gmail.com>
2022-10-30 22:16:47 -0700

    Re-organize

--- solutions/rust/src/leetcode/mod.rs
+++ solutions/rust/src/leetcode/mod.rs
@@ -1,1 +1,2 @@
-pub mod p509;
+#[path="509-fibonacci_number.rs"]
+pub mod s509_fibonacci_number;
--- solutions/rust/src/main.rs
+++ solutions/rust/src/main.rs
@@ -1,14 +1,16 @@
 use std::io;
 use std::time::{SystemTime};
 mod leetcode;
-use leetcode::p509::solution::Solution;
+use leetcode::s509_fibonacci_number::Solution;
 
 fn main() {
     let mut user_input = String::new();
     println!("Enter LeetCode problem #:");
-    io::stdin().read_line(&mut user_input).expect("Error: Unable to read user input");
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Error: Unable to read user input");
     let start = SystemTime::now();
-    let solution = Solution::fib();
+    let solution = Solution::fib(2);
     let end = SystemTime::now();
     let duration = end.duration_since(start).unwrap();
     println!("Solution: {}", solution);
