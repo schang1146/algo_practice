85511085217c05e41f1c4485ac9a4ad8bd0b0b9c
schang1146 <sammychang2185@gmail.com>
2022-11-14 22:02:12 -0800

    Allow load problem to write to mod file in build

---
+++ solutions/rust/src/build/mod.rs
@@ -0,0 +1,1 @@
+mod s509_fibonacci_number.rs;
--- solutions/rust/src/load.rs
+++ solutions/rust/src/load.rs
@@ -1,18 +1,26 @@
 // use std::ffi::OsStr;
 use std::fs;
+use std::io::Write;
 // use std::path::Path;
 
 pub fn load_problem(id: &String) {
     let base_path = "./src/";
+    let mod_path = "./src/build/mod.rs";
 
     // let mut source_dest: &Path;
     // let target_dest: &Path = Path::new(format!("{base_path}foo.txt").as_str());
 
     for file in fs::read_dir(format!("{base_path}leetcode").as_str()).unwrap() {
         let file_path = file.unwrap().path();
-        let file_name = file_path.file_name().unwrap();
-        let test: Vec<&str> = file_name.to_str().unwrap().split("-").collect();
-        if test[0].eq(id.trim()) {
+        let file_name = file_path.file_name().unwrap().to_str().unwrap();
+        let test: Vec<&str> = file_name.split("_").collect();
+        if test[0].eq(format!("s{id}").as_str()) {
+            let mod_file = fs::OpenOptions::new()
+                .write(true)
+                .truncate(true)
+                .open(mod_path)
+                .unwrap();
+            writeln!(&mod_file, "mod {};", file_name).unwrap();
             println!("Solution found!");
             // source_dest = Path::new(file_path.as_path())
         }
