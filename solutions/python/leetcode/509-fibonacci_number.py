3aa23bc2655c5c2d9b81d5caaa6867430d19ecd8
schang1146 <20074034+schang1146@users.noreply.github.com>
2023-07-04 17:35:42 -0400

    Remove timing and some general spacing

--- solutions/python/leetcode/509-fibonacci_number.py
+++ solutions/python/leetcode/509-fibonacci_number.py
@@ -1,6 +1,3 @@
-from time import perf_counter
-
-
 class Solution:
     def fib(self, n: int) -> int:
         fib_data = [0, 1, 1]
@@ -8,13 +5,11 @@ class Solution:
             raise Exception("Error: Can't get negative Fibonacci numbers.")
         if n <= 2:
             return fib_data[n]
-        for _ in range(n-2):
+        for _ in range(n - 2):
             fib_data = fib_data[1:] + [fib_data[1] + fib_data[2]]
         return fib_data[-1]
 
+
 if __name__ == "__main__":
-    tic = perf_counter()
     solution = Solution().fib(2)
-    toc = perf_counter()
     print(f"Solution: {solution}")
-    print(f"Time elapsed: {toc - tic} sec(s)")
